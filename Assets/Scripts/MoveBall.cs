51b549cb69e2869ccbb7338b0204e89a815302fb
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-09-13 12:25:03 -0300

    Made a score, UI, and win message

--- Assets/Scripts/MoveBall.cs
+++ Assets/Scripts/MoveBall.cs
@@ -2,20 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class MoveBall : MonoBehaviour
 {
 
     public float Speed;
+    public TextMeshProUGUI ScoreText;
+    public int MaxScore;
+    public GameObject WinMessage;
 
     private Rigidbody rb;
     private float MovementX;
     private float MovementY;
 
+    private int ScoreCount;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        ScoreCount = 0;
+        SetScoreText();
+        WinMessage.SetActive(false);
     }
 
     void OnMove(InputValue MovementValue)
@@ -30,6 +39,25 @@ public class MoveBall : MonoBehaviour
         Vector3 Move = new Vector3(MovementX, 0, MovementY);
 
         rb.AddForce(Move * Speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectible"))
+        {
+            other.gameObject.SetActive(false);
+            ScoreCount++;
+            SetScoreText();
+        }
+    }
+
+    void SetScoreText()
+    {
+        ScoreText.text = "Score: " + ScoreCount.ToString();
+        if(ScoreCount >= MaxScore)
+        {
+            WinMessage.SetActive(true);
+        }
     }
 
 }
